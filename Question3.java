23b13359888c1b1b1f3ab0ba1b2a14871abf7a3d
Shubham Kaushik <66999765+ShubhamKaushikkk@users.noreply.github.com>
2022-10-18 23:15:02 +0530

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -1,11 +1,16 @@
 import java.util.Arrays;
 public class Question3 {
     public static void main(String []args){
-        String s= "interstellar";
-        char []arr= new char[20];
-        arr=s.toCharArray();
-        Arrays.sort(arr);
-        String c= new String(arr);
-        System.out.println(c);
+        String[] s= {"Shubham","Ayush","Raghav","Aayush","Bhupati"};
+        for(int i=0;i<s.length;i++) {
+            for(int j=i+1;j<s.length;j++) {
+                if(s[i].compareTo(s[j])>0) {
+                    String temp= s[i];
+                    s[i]= s[j];
+                    s[j]= temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(s));
     }
 }
